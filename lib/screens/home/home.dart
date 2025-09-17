5dfa64def2d3445dba8ae8b43523920735b629ad
Saiful-Bahri-95 <saiful.bahri2004@gmail.com>
2025-09-17 09:59:46 +0700

    telkomsel-app-update1

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:telkomsel_app/screens/home/components/card_info_home_1.dart';
 import 'package:telkomsel_app/themes.dart';
 
 class HomePage extends StatelessWidget {
@@ -69,122 +69,7 @@ class HomePage extends StatelessWidget {
     }
 
     Widget cardInfo() {
-      return Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 18, left: 16, right: 16),
-            height: 333,
-            decoration: BoxDecoration(
-              color: whiteColor,
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Column(
-              children: [
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: greyCardColor,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15),
-                      ),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 17, bottom: 21, top: 27),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Rp 150.999,-',
-                            style: extraBoldText26.copyWith(
-                              color: primaryTextColor,
-                            ),
-                          ),
-                          Text(
-                            'Active until 22 Sep 2025',
-                            style: mediumText12.copyWith(color: darkGreyColor),
-                          ),
-                          Spacer(),
-                          Row(
-                            children: [
-                              Container(
-                                padding: EdgeInsets.symmetric(
-                                  vertical: 7,
-                                  horizontal: 19,
-                                ),
-                                decoration: BoxDecoration(
-                                  color: redButtonColor,
-                                  borderRadius: BorderRadius.circular(16.5),
-                                ),
-                                child: Text(
-                                  'Buy Package',
-                                  style: mediumText12.copyWith(
-                                    color: whiteColor,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(width: 18),
-                              Text(
-                                'Top-Up',
-                                style: mediumText12.copyWith(color: blueColor),
-                              ),
-                              SizedBox(width: 19),
-                              Text(
-                                'Send Gift',
-                                style: mediumText12.copyWith(color: blueColor),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            flex: 1,
-                            child: CircularPercentIndicator(
-                              radius: 34.0,
-                              lineWidth: 7.0,
-                              animation: true,
-                              percent: 0,
-                              circularStrokeCap: CircularStrokeCap.round,
-                              backgroundColor: lightGreyColor,
-                              progressColor: orangeColor,
-                            ),
-                          ),
-                          Expanded(
-                            flex: 7,
-                            child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Text(
-                                      'Internet',
-                                      style: boldText13.copyWith(
-                                        color: darkGreyColor,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      );
+      return Column(children: [CardInfoHome()]);
     }
 
     return Scaffold(
